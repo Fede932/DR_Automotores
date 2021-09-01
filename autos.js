28aa02026a1adedc0226d4259066c81f7cb6cb8d
Javier Mendez <mendezjavierdario@gmail.com>
2021-08-31 22:02:29 -0300

    se agrega search box

---
+++ autos.js
@@ -0,0 +1,32 @@
+const autos = [
+  {
+    id: 1,
+    logo: "https://www.excelenciasdelmotor.com/sites/default/files/u7/chevrolet-logo.jpg",
+    marca: "Chevrolet",
+    url: "autos.html",
+  },
+  {
+    id: 3,
+    logo: "https://1000marcas.net/wp-content/uploads/2020/01/Ford-logo.png",
+    marca: "Ford",
+    url: "autos-2.html",
+  },
+  {
+    id: 4,
+    logo: "",
+    marca: "Volkswagen",
+    url: "autos-3.html",
+  },
+  {
+    id: 4,
+    logo: "",
+    marca: "Corolla",
+    url: "autos-3.html",
+  },
+  {
+    id: 4,
+    logo: "",
+    marca: "Peugeot",
+    url: "autos-3.html",
+  },
+];
